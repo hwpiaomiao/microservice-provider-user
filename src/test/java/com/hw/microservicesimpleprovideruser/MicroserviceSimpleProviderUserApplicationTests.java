c3e5d6bc4eac10b77594a33f1841f8a82f188679
huangwen <506300372@qq.com>
2018-10-16 01:18:43 +0800

    Initial commit

---
+++ src/test/java/com/hw/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplicationTests.java
@@ -0,0 +1,14 @@
+/*package com.hw.microservicesimpleprovideruser;
+// SpringJUnit支持，由此引入Spring-Test框架支持！
+@RunWith(SpringJUnit4ClassRunner.class)
+// 指定我们SpringBoot工程的Application启动类
+@SpringApplicationConfiguration(classes = EurakeServerApplication.class)
+//由于是Web项目，Junit需要模拟ServletContext，因此我们需要给我们的测试类加上@WebAppConfiguration。
+@WebAppConfiguration
+public class MicroserviceSimpleProviderUserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}*/
